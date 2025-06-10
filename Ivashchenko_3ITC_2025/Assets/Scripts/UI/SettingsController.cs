99cefec10f51a74524e226f01d685991e92c1b02
Mikhail Ivashchenko <52649381+mikhail0090025@users.noreply.github.com>
2025-06-10 11:03:41 +0200

    update

--- Ivashchenko_3ITC_2025/Assets/Scripts/UI/SettingsController.cs
+++ Ivashchenko_3ITC_2025/Assets/Scripts/UI/SettingsController.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Rendering;
 public class SettingsController : MonoBehaviour
 {
     // Settings
     static int currentResolution = 0;
     static bool fullscreen = true;
+    static float volume = 1.0f;
     // UI
     [SerializeField] TMP_Dropdown ResolutionDropdown;
     [SerializeField] Toggle FullscreenToggle;
+    [SerializeField] Slider VolumeSlider;
+    [SerializeField] TMP_Text VolumePercent;
     void Start()
     {
         ResolutionDropdown.value = currentResolution;
         FullscreenToggle.isOn = fullscreen;
+        VolumeSlider.value = volume;
         ResolutionDropdown.onValueChanged.AddListener(delegate
         {
             SetSettings();
@@ -25,12 +30,19 @@ public class SettingsController : MonoBehaviour
             SetSettings();
             ApplySettings();
         });
+        VolumeSlider.onValueChanged.AddListener(delegate
+        {
+            SetSettings();
+            ApplySettings();
+        });
     }
 
     void SetSettings()
     {
         currentResolution = ResolutionDropdown.value;
         fullscreen = FullscreenToggle.isOn;
+        volume = VolumeSlider.value;
+        VolumePercent.text = ((int)(VolumeSlider.value * 100f)).ToString() + "%";
     }
     void ApplySettings()
     {
@@ -52,5 +64,6 @@ public class SettingsController : MonoBehaviour
                 Screen.SetResolution(640, 480, fullscreen);
                 break;
         }
+        
     }
 }
